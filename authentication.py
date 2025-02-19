0b50167d3227a0f3bd5e37a45dc0a1cb7bff59f8
Arnav Goel <arnavgoel13@gmail.com>
2025-02-19 17:41:47 -0600

    Implemented login_service function

--- authentication.py
+++ authentication.py
@@ -11,3 +11,22 @@ def verify_credentials(username, password):
         return bcrypt.checkpw(password.encode(), hash_value)  # Checks against stored hash value
     return False  # Credentials are incorrect
 
+
+def login_service():
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+    print("Login service started on port 5555....")
+
+    while True:
+        message = socket.recv()
+        data = json.loads(message.decode())
+        username = data.get("username")
+        password = data.get("password")
+
+        success_code = verify_credentials(username, password)
+        if success_code:
+            response = {'status: successfully verified'}
+        else:
+            response = {'status: unable to verify login credentials'}
+        socket.send(json.dumps(response).encode())
